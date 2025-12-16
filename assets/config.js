afa047853799c565fb1a564a9e6e90907ad00bb8
gerenciabeelluv2-cloud <gerenciabeelluv2@gmail.com>
2025-12-16 18:32:58 -0300

    Delete assets directory

--- assets/config.js
+++
@@ -1,8 +0,0 @@
-export const prints = {
-  hero: "assets/hero-dashboard.png",
-  creators: "assets/creators-screen.png",
-  employees: "assets/employees-screen.png",
-  shifts: "assets/shifts-screen.png",
-  logo: "assets/logo-beeflow.png",
-};
-
